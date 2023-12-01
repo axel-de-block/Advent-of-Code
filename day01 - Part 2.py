0071651bf66dcd84c7d3e12ec06adff71bde029a
Axel <44074386+Silterman@users.noreply.github.com>
2023-12-01 23:19:50 +0100

    optimization
    removed the 9 .replace calls and went for a more methodical approach to checking for written numbers

--- day01 - Part 2.py
+++ day01 - Part 2.py
@@ -15,8 +15,16 @@ def filterFunc(i):
     return False
 
 for line in lines:
-    line = line.replace("one", "one1one").replace("two", "two2two").replace("three", "three3three").replace("four", "four4four").replace("five", "five5five").replace("six", "six6six").replace("seven", "seven7seven").replace("eight", "eight8eight").replace("nine", "nine9nine")
-    line = list(filter(filterFunc, list(line)))
-    nums.append(int(line[0]+line[-1]))
+    filteredLine = []
+    for i, letter in enumerate(line):
+        if letter.isdigit():
+            filteredLine.append(letter)
+            continue
+        for num, wNum in enumerate(['one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine']):
+            if line[i:].startswith(wNum):
+                filteredLine.append(str(num+1))
+                break
+
+    nums.append(int(filteredLine[0]+filteredLine[-1]))
     
 print(sum(nums))
