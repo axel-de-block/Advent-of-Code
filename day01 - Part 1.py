4f10e3a6b06b636163e6c80396d65a269151cf94
Axel <44074386+Silterman@users.noreply.github.com>
2023-12-01 23:13:24 +0100

    changed list check to .isdigit()

--- day01 - Part 1.py
+++ day01 - Part 1.py
@@ -1,11 +1,10 @@
-with open("TextInputs\day01.txt", "r") as open_doc:
+with open("TextInputs\\day01.txt", "r") as open_doc:
     lines = [line.replace("\n", "") for line in open_doc.readlines()]
 
 nums = []
 
 def filterFunc(i):
-    numL = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"]
-    if i in numL:
+    if i.isdigit():
         return True
     return False
 
--- day01 - Part 2.py
+++ day01 - Part 2.py
@@ -10,15 +10,13 @@ with open("TextInputs\\day01.txt", "r") as open_doc:
 nums = []
 
 def filterFunc(i):
-    numL = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"]
-    if i in numL:
+    if i.isdigit():
         return True
     return False
 
 for line in lines:
     line = line.replace("one", "one1one").replace("two", "two2two").replace("three", "three3three").replace("four", "four4four").replace("five", "five5five").replace("six", "six6six").replace("seven", "seven7seven").replace("eight", "eight8eight").replace("nine", "nine9nine")
     line = list(filter(filterFunc, list(line)))
-    print(line, int(line[0]+line[-1]))
     nums.append(int(line[0]+line[-1]))
     
 print(sum(nums))
