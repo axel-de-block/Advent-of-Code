c8d6f96c809ffe8d6908548ab04cb440e87d8e2c
Axel <44074386+Silterman@users.noreply.github.com>
2023-12-02 21:05:36 +0100

    prepped for day 3

---
+++ day03.py
@@ -0,0 +1,8 @@
+with open("TextInputs\\day01.txt", "r") as open_doc:
+    lines = [line.replace("\n", "") for line in open_doc.readlines()]
+    
+"""
+with open("TextInputs\\testCases.txt", "r") as open_doc:
+    lines = [line.replace("\n", "") for line in open_doc.readlines()]
+
+"""
