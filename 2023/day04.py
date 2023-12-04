261ebcaa61ee087aceaec44782a514c226c15053
Axel <44074386+Silterman@users.noreply.github.com>
2023-12-04 16:43:27 +0100

    removed redudancy
    .split() != .split(" ")

--- 2023/day04.py
+++ 2023/day04.py
@@ -18,16 +18,11 @@ for line in lines:
 
 cardsQuantity = [1]*len(lines) #part 2
 
-def removeEmpty(list: list) -> list: 
-    while "" in list:
-        list.remove("")
-    return list
-
 for i, card in enumerate(cards):
     curPoints = 0.5
     matchingNumbers = 0
-    winningNumbers = removeEmpty(card[0].split(" "))
-    potentialNumbers = removeEmpty(card[1].split(" "))
+    winningNumbers = card[0].split()
+    potentialNumbers = card[1].split()
     for number in winningNumbers:
         if number in potentialNumbers:
             curPoints *= 2
