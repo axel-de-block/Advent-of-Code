b66ce49f56688a5485beb727ccc72bbffdac7967
Axel <44074386+Silterman@users.noreply.github.com>
2023-12-04 15:55:59 +0100

    day 4 complete
    parts 1 and 2 are in the same file

---
+++ 2023/day04.py
@@ -0,0 +1,40 @@
+from math import floor
+
+with open("TextInputs\\day04.txt", "r") as open_doc:
+    lines = [line.replace("\n", "") for line in open_doc.readlines()]
+    
+"""
+with open("TextInputs\\testCases.txt", "r") as open_doc:
+    lines = [line.replace("\n", "") for line in open_doc.readlines()]
+"""
+
+
+points = 0
+
+cards = []
+
+for line in lines:
+    cards.append(line[line.index(":")+2:].split(" | "))
+
+cardsQuantity = [1]*len(lines) #part 2
+
+def removeEmpty(list: list) -> list: 
+    while "" in list:
+        list.remove("")
+    return list
+
+for i, card in enumerate(cards):
+    curPoints = 0.5
+    matchingNumbers = 0
+    winningNumbers = removeEmpty(card[0].split(" "))
+    potentialNumbers = removeEmpty(card[1].split(" "))
+    for number in winningNumbers:
+        if number in potentialNumbers:
+            curPoints *= 2
+            matchingNumbers += 1
+    for j in range(matchingNumbers):
+        cardsQuantity[i+j+1] += 1*cardsQuantity[i] 
+    points += int(floor(curPoints))
+
+print(cardsQuantity)
+print(points, sum(cardsQuantity))
