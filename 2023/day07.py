1b02d705233f59a6bb95892c9577c75454519a49
Axel <44074386+Silterman@users.noreply.github.com>
2023-12-07 18:50:11 +0100

    day 7 part 1 complete
    I'm a regard

--- 2023/day07.py
+++ 2023/day07.py
@@ -9,80 +9,35 @@ with open("TextInputs\\testCases.txt", "r") as open_doc:
 
 winnings = 0
 
-combos = [[] for _ in range(6)] #fives, fours, houses, threes, twos and ones
+combos = [[] for _ in range(7)]
 sortedCombos = []
 
-#sorts hands in their respective category -> now I need a sorted of those categories 
+def typeOfHand(hand:list[int]) -> int:
+    handSet = list(set(hand))
+    if len(handSet) == 5: #high card
+        return 0
+    if len(handSet) == 1: #five of a kind
+        return 6 
+    if len(handSet) == 2:
+        if hand.count(handSet[0]) in [1, 4]:
+            return 5 #four of a kind
+        return 4 #full house
+    if len(handSet) == 3:
+        if max(hand.count(handSet[0]), hand.count(handSet[1]), hand.count(handSet[2])) == 3:
+            return 3 #thee of a kind
+        return 2 #two of a kind
+    return 1 #one of a kind  
+
 for line in lines:
     skip = False
     hand, bid = line.split()
-    hand = ["0123456789TJQKA".index(i) for i in hand]
-    setHand = list(set(hand))
-    if len(setHand) == 1: #five of a kind
-        combos[5].append([hand, int(bid)])
-    elif len(setHand) == 5: #high card
-        continue
-    elif len(setHand) == 4: #one of a kind
-        combos[0].append([hand, int(bid)])
-    elif max([hand.count(card) for card in hand]) == 4: #four of a kind
-        combos[4].append([hand, int(bid)])
-    elif len(setHand) == 2: #Full house
-        combos[3].append([hand, int(bid)])
-    elif max([hand.count(card) for card in hand]) == 3: #three of a kind
-        combos[2].append([hand, int(bid)])
-    else: #two of a kind
-        combos[1].append([hand, int(bid)])    
-        
+    hand = ["23456789TJQKA".index(i) for i in hand]
+    combos[typeOfHand(hand)].append([hand, int(bid)])
+               
+i = 1
 for group in combos:
-    sortedCombos.extend([i[1] for i in sorted(group)])
-
-for i, num in enumerate(sortedCombos, 1):
-    winnings += i*num
+    for item in sorted(group):
+        winnings += i*item[1]
+        i += 1
     
 print(winnings)
-
-#cheating
-
-with open('TextInputs\\day07.txt') as f:
-    data = f.read().splitlines()
-
-data = [line.split() for line in data]
-print(data)
-
-cards = '2 3 4 5 6 7 8 9 T J Q K A'.split()
-strength_dict = {card:index for index,card in enumerate(cards)}
-sorted_hands = []
-hand_types = [[] for _ in range(7)]
-
-def find_hand_type(hand,hand_set):
-    #5 of a kind
-    if len(hand_set) == 1:
-        return 6
-    #1 pair
-    elif len(hand_set) == 4:
-        return 1
-    #High card
-    elif len(hand_set) == 5:
-        return 0
-    #4 of a kind
-    elif max([hand.count(card) for card in hand]) == 4:
-        return 5
-    #Full house
-    elif len(hand_set) == 2:
-        return 4
-    #3 of a kind
-    elif max([hand.count(card) for card in hand]) == 3:
-        return 3
-    return 2
-
-for info in data:
-    hand = list(info[0])
-    hand_set = set(hand)
-    hand_types[find_hand_type(hand,hand_set)].append(info)
-    
-for hand_type in hand_types:
-    print(sorted(hand_type,key=lambda info: [strength_dict[card] for card in info[0]]))
-    sorted_hands.extend((sorted(hand_type,key=lambda info: [strength_dict[card] for card in info[0]])))
-
-part1 = sum([index * int(info[1]) for index,info in enumerate(sorted_hands,1)])
-print(part1)
